76becb5f6c3b5ff1f3fbf06db7ca9b8cbeb9fba0
Pruthviraj-Patil-1434 <pruthvirajpatil1434@gamil.com>
2026-05-05 15:19:15 +0530

    Login Project By Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('Login');
+  email: string = '';
+  pass: string = '';
+
+  onLogin() {
+    if (this.email === 'pruthvi21@gmail.com' && this.pass === '1234') {
+      alert('Login Successful');
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 }
